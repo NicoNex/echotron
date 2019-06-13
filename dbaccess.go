f7be4b6dbc9a41cefbf8fb038f0e28d3651149dd
Dj_Mike238 <mike238dimaggio@gmail.com>
2019-06-13 16:36:15 +0200

    DBAccess: added documentation

--- dbaccess.go
+++ dbaccess.go
@@ -26,6 +26,9 @@ import (
 	_ "github.com/go-sql-driver/mysql"
 )
 
+// DB is the struct which contains the hostname of the database,
+// the login data and the name of the database that will be used
+// for the various operations.
 type DB struct {
 	host string `json:"host"`
 	user string `json:"user"`
@@ -34,6 +37,10 @@ type DB struct {
 }
 
 
+// NewDB creates a new DB instance.
+// It reads the data needed by the DB struct from a file (credFile),
+// stores them in the DB struct, tries to create a new database if
+// it doesn't exist and returns a DB instance.
 func NewDB(credFile string) (*DB, error) {
 	jsonFile, err := ioutil.ReadFile(credFile)
 
@@ -102,31 +109,38 @@ func (db *DB) createDB() {
 }
 
 
+// CreateTable creates a new table in the database.
 func (db *DB) CreateTable(name string, columns string) (string, error) {
 	return db.executeQuery(fmt.Sprintf("CREATE TABLE %s (%s) DEFAULT CHARSET=utf8", name, columns))
 }
 
 
+// AddColumnToTable adds a new column to a specific table.
 func (db *DB) AddColumnToTable(tableName string, columnName string) (string, error) {
 	return db.executeQuery(fmt.Sprintf("ALTER TABLE %s ADD COLUMN %s", tableName, columnName))
 }
 
 
+// InsertRecord inserts data into the database.
 func (db *DB) InsertRecord(tableName string, columns string, values string) (string, error) {
 	return db.executeQuery(fmt.Sprintf("INSERT INTO %s (%s) VALUES (\"%s\")", tableName, columns, values))
 }
 
 
+// SelectRecord reads data from the database and returns it.
 func (db *DB) SelectRecord(tableName string, columnName string, value string) (string, error) {
 	return db.executeQuery(fmt.Sprintf("SELECT %s FROM %s WHERE %s = \"%s\"", columnName, tableName, columnName, value))
 }
 
 
+// Similar to SelectRecord, but in addition allows
+// to compare some known data with a specific column.
 func (db *DB) SelectRecordCompared(tableName string, columnName string, columnToCompare string, value string) (string, error) {
 	return db.executeQuery(fmt.Sprintf("SELECT %s FROM %s WHERE %s = \"%s\"", columnName, tableName, columnToCompare, value))
 }
 
 
+// TableExists is used to check whether a specific table exists.
 func (db *DB) TableExists(tableName string) bool {
 	queryResult, err := db.executeQuery(fmt.Sprintf("SELECT table_name FROM information_schema.tables WHERE table_name = \"%s\"", tableName))
 
@@ -138,11 +152,13 @@ func (db *DB) TableExists(tableName string) bool {
 }
 
 
+// UpdateRecord edits some specific data in the database.
 func (db *DB) UpdateRecord(tableName string, columnName1 string, value1 string, columnName2 string, value2 string) (string, error) {
 	return db.executeQuery(fmt.Sprintf("UPDATE %s SET %s = \"%s\" WHERE %s = \"%s\"", tableName, columnName1, value1, columnName2, value2))
 }
 
 
+// DeleteRecord deletes some specific data from the database.
 func (db *DB) DeleteRecord(tableName string, columnName string, value string) (string, error) {
 	return db.executeQuery(fmt.Sprintf("DELETE FROM %s WHERE %s = \"%s\"", tableName, columnName, value))
 }
