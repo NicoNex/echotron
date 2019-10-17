9c8d5777d5cb81fc99bfa12a13ea79196a8b8381
Dj_Mike238 <mike238dimaggio@gmail.com>
2019-10-17 12:42:06 +0200

    Revert "types.go: update APIResponse"
    This reverts commit e372d0f4e06e07b99069bf37f7a3fcccf1d3a997.

--- types.go
+++ types.go
@@ -91,10 +91,10 @@ type Update struct {
 
 // This object represents the incoming response from Telegram servers.
 type APIResponse struct {
-	Ok          bool       `json:"ok"`
-	Result      []*Message `json:"result,omitempty"`
-	ErrorCode   int        `json:"error_code,omitempty"`
-	Description string     `json:"description,omitempty"`
+	Ok          bool      `json:"ok"`
+	Result      []*Update `json:"result,omitempty"`
+	ErrorCode   int       `json:"error_code,omitempty"`
+	Description string    `json:"description,omitempty"`
 }
 
 // This object represents a phone contact.
