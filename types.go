6f789659b86ebad6ec9b830c850b323fbdcf5d72
Dj_Mike238 <mike238dimaggio@gmail.com>
2019-10-25 09:26:14 +0200

    types.go: gofmt'd

--- types.go
+++ types.go
@@ -101,10 +101,10 @@ type APIResponseUpdate struct {
 // This object represents the incoming response from Telegram servers.
 // Used by the methods in the api.go module (since they return a Message).
 type APIResponseMessage struct {
-	Ok          bool       `json:"ok"`
-	Result      *Message   `json:"result,omitempty"`
-	ErrorCode   int        `json:"error_code,omitempty"`
-	Description string     `json:"description,omitempty"`
+	Ok          bool     `json:"ok"`
+	Result      *Message `json:"result,omitempty"`
+	ErrorCode   int      `json:"error_code,omitempty"`
+	Description string   `json:"description,omitempty"`
 }
 
 // This object represents a phone contact.
