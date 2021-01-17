f6041998a48c233a33979e00935380baf4b925b3
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2021-01-17 09:32:36 +0100

    Make the dispatcher more modular.

--- dispatcher.go
+++ dispatcher.go
@@ -31,17 +31,21 @@ type Dispatcher struct {
 	api        Api
 	sessionMap map[int64]Bot
 	newBot     NewBotFn
+	updates    chan []*Update
 }
 
 // NewDispatcher returns a new instance of the Dispatcher object;
 // useful for polling telegram and dispatch every update to the
 // corresponding Bot instance.
 func NewDispatcher(token string, newBot NewBotFn) Dispatcher {
-	return Dispatcher{
+	d := Dispatcher{
 		NewApi(token),
 		make(map[int64]Bot),
 		newBot,
+		make(chan []*Update),
 	}
+	go d.listen()
+	return d
 }
 
 // DelSession deletes the Bot instance, seen as a session, from the
@@ -57,49 +61,56 @@ func (d *Dispatcher) AddSession(chatId int64) {
 	}
 }
 
-// Run starts the polling loop and calls the function Update
+// Poll starts the polling loop so that the dispatcher calls the function Update
 // upon receiving any update from Telegram.
-func (d *Dispatcher) Run() {
+func (d *Dispatcher) Poll() {
 	var timeout int
-	var chatId int64
-	var response APIResponseUpdate
-
 	var firstRun = true
 	var lastUpdateId = -1
 
 	for {
-		response = d.api.GetUpdates(lastUpdateId+1, timeout)
+		response := d.api.GetUpdates(lastUpdateId+1, timeout)
 		if response.Ok {
-			for _, update := range response.Result {
-				lastUpdateId = update.ID
+			if !firstRun {
+				d.updates <- response.Result
+			}
 
-				if update.Message != nil {
-					chatId = update.Message.Chat.ID
-				} else if update.EditedMessage != nil {
-					chatId = update.EditedMessage.Chat.ID
-				} else if update.ChannelPost != nil {
-					chatId = update.ChannelPost.Chat.ID
-				} else if update.EditedChannelPost != nil {
-					chatId = update.EditedChannelPost.Chat.ID
-				} else {
-					continue
-				}
-
-				if _, isIn := d.sessionMap[chatId]; !isIn {
-					d.sessionMap[chatId] = d.newBot(chatId)
-				}
-
-				if !firstRun {
-					if bot, ok := d.sessionMap[chatId]; ok {
-						go bot.Update(update)
-					}
-				}
+			if l := len(response.Result); l > 0 {
+				lastUpdateId = response.Result[l-1].ID
 			}
 		}
 
 		if firstRun {
 			firstRun = false
 			timeout = 120
+		}
+	}
+}
+
+func (d *Dispatcher) listen() {
+	for uList := range d.updates {
+		for _, update := range uList {
+			var chatId int64
+
+			if update.Message != nil {
+				chatId = update.Message.Chat.ID
+			} else if update.EditedMessage != nil {
+				chatId = update.EditedMessage.Chat.ID
+			} else if update.ChannelPost != nil {
+				chatId = update.ChannelPost.Chat.ID
+			} else if update.EditedChannelPost != nil {
+				chatId = update.EditedChannelPost.Chat.ID
+			} else {
+				continue
+			}
+
+			if _, isIn := d.sessionMap[chatId]; !isIn {
+				d.sessionMap[chatId] = d.newBot(chatId)
+			}
+
+			if bot, ok := d.sessionMap[chatId]; ok {
+				go bot.Update(update)
+			}
 		}
 	}
 }
