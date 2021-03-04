1685fd9ccb142ecacb06e4a93ce706259c0aacc3
Dj_Mike238 <22237379+DjMike238@users.noreply.github.com>
2021-03-04 01:11:50 +0100

    Add types_inline.go
    Add types for inline bots

--- types.go
+++ types.go
@@ -129,23 +129,6 @@ type Location struct {
 	Latitude  float32
 }
 
-// This object represents an incoming inline query.
-// When the user sends an empty query, your bot could return some default or trending results.
-type InlineQuery struct {
-	ID     string `json:"id"`
-	User   *User  `json:"user"`
-	Query  string `json:"query"`
-	Offset string `json:"offset"`
-}
-
-// Represents a result of an inline query that was chosen by the user and sent to their chat partner.
-type ChosenInlineResult struct {
-	ID              string `json:"result_id"`
-	User            *User  `json:"user"`
-	InlineMessageId string `json:"inline_message_id,omitempty"`
-	Query           string `json:"query,omitempty"`
-}
-
 // This object represents an incoming callback query from a callback button in an inline keyboard.
 // If the button that originated the query was attached to a message sent by the bot,
 // the field message will be present. If the button was attached to a message sent via the bot (in inline mode),
