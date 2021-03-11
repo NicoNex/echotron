b41c11648784d8413003e01d28a662a7ea407a48
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2021-03-12 00:24:02 +0100

    Code refactor and add comments.

--- dispatcher.go
+++ dispatcher.go
@@ -26,6 +26,7 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"sync"
 )
 
 // Bot is the interface that must be implemented by your definition of
@@ -35,24 +36,30 @@ type Bot interface {
 	Update(*Update)
 }
 
+// NewBotFn is called every time echotron receives an update with a chat ID never
+// encountered before.
 type NewBotFn func(chatId int64) Bot
 
+// The Dispatcher passes the updates from the Telegram Bot Api to the Bot instance
+// associated with each chatID. When a new chat ID is found, the provided function
+// of type NewBotFn will be called.
 type Dispatcher struct {
 	api        Api
 	sessionMap map[int64]Bot
 	newBot     NewBotFn
 	updates    chan *Update
+	mu         sync.Mutex
 }
 
-// NewDispatcher returns a new instance of the Dispatcher object;
-// useful for polling telegram and dispatch every update to the
-// corresponding Bot instance.
-func NewDispatcher(token string, newBot NewBotFn) Dispatcher {
-	d := Dispatcher{
-		NewApi(token),
-		make(map[int64]Bot),
-		newBot,
-		make(chan *Update),
+// NewDispatcher returns a new instance of the Dispatcher object.
+// Calls the Update function of the bot associated with each chat ID.
+// If a new chat ID is found, newBotFn will be called first.
+func NewDispatcher(token string, newBotFn NewBotFn) *Dispatcher {
+	d := &Dispatcher{
+		api:        NewApi(token),
+		sessionMap: make(map[int64]Bot),
+		newBot:     newBotFn,
+		updates:    make(chan *Update),
 	}
 	go d.listen()
 	return d
@@ -60,15 +67,19 @@ func NewDispatcher(token string, newBot NewBotFn) Dispatcher {
 
 // DelSession deletes the Bot instance, seen as a session, from the
 // map with all of them.
-func (d *Dispatcher) DelSession(chatId int64) {
-	delete(d.sessionMap, chatId)
+func (d *Dispatcher) DelSession(chatID int64) {
+	d.mu.Lock()
+	delete(d.sessionMap, chatID)
+	d.mu.Unlock()
 }
 
 // AddSession allows to arbitrarily create a new Bot instance.
-func (d *Dispatcher) AddSession(chatId int64) {
-	if _, isIn := d.sessionMap[chatId]; !isIn {
-		d.sessionMap[chatId] = d.newBot(chatId)
+func (d *Dispatcher) AddSession(chatID int64) {
+	d.mu.Lock()
+	if _, isIn := d.sessionMap[chatID]; !isIn {
+		d.sessionMap[chatID] = d.newBot(chatID)
 	}
+	d.mu.Unlock()
 }
 
 // Poll starts the polling loop so that the dispatcher calls the function Update
@@ -76,18 +87,18 @@ func (d *Dispatcher) AddSession(chatId int64) {
 func (d *Dispatcher) Poll() error {
 	var timeout int
 	var firstRun = true
-	var lastUpdateId = -1
+	var lastUpdateID = -1
 
 	// deletes webhook if present to run in long polling mode
 	response, err := d.api.DeleteWebhook()
 	if err != nil {
 		return err
 	} else if !response.Ok {
-		return errors.New("could not disable webhook, running in long polling mode is not possible.")
+		return errors.New("could not disable webhook, running in long polling mode is not possible")
 	}
 
 	for {
-		response, err := d.api.GetUpdates(lastUpdateId+1, timeout)
+		response, err := d.api.GetUpdates(lastUpdateID+1, timeout)
 		if err != nil {
 			return err
 		} else if response.Ok {
@@ -98,7 +109,7 @@ func (d *Dispatcher) Poll() error {
 			}
 
 			if l := len(response.Result); l > 0 {
-				lastUpdateId = response.Result[l-1].ID
+				lastUpdateID = response.Result[l-1].ID
 			}
 		}
 
@@ -107,75 +118,83 @@ func (d *Dispatcher) Poll() error {
 			timeout = 120
 		}
 	}
-
-	return nil
 }
 
 func (d *Dispatcher) listen() {
 	for update := range d.updates {
-		var chatId int64
+		var chatID int64
 
 		if update.Message != nil {
-			chatId = update.Message.Chat.ID
+			chatID = update.Message.Chat.ID
 		} else if update.EditedMessage != nil {
-			chatId = update.EditedMessage.Chat.ID
+			chatID = update.EditedMessage.Chat.ID
 		} else if update.ChannelPost != nil {
-			chatId = update.ChannelPost.Chat.ID
+			chatID = update.ChannelPost.Chat.ID
 		} else if update.EditedChannelPost != nil {
-			chatId = update.EditedChannelPost.Chat.ID
+			chatID = update.EditedChannelPost.Chat.ID
 		} else if update.CallbackQuery != nil {
-			chatId = update.CallbackQuery.Message.Chat.ID
+			chatID = update.CallbackQuery.Message.Chat.ID
 		} else if update.InlineQuery != nil {
-			chatId = update.InlineQuery.From.ID
+			chatID = update.InlineQuery.From.ID
 		} else {
 			continue
 		}
 
-		if _, isIn := d.sessionMap[chatId]; !isIn {
-			d.sessionMap[chatId] = d.newBot(chatId)
+		d.mu.Lock()
+		if _, isIn := d.sessionMap[chatID]; !isIn {
+			d.sessionMap[chatID] = d.newBot(chatID)
 		}
 
-		if bot, ok := d.sessionMap[chatId]; ok {
+		if bot, ok := d.sessionMap[chatID]; ok {
 			go bot.Update(update)
 		}
+		d.mu.Unlock()
 	}
 }
 
 // ListenWebhook sets a webhook and listens for incoming updates
-func (d *Dispatcher) ListenWebhook(url string, internalPort int) error {
+func (d *Dispatcher) ListenWebhook(webhookURL, listenURL string, internalPort int) error {
 	var response APIResponseUpdate
 
-	response, err := d.api.SetWebhook(url)
+	response, err := d.api.SetWebhook(webhookURL)
 	if err != nil {
 		return err
 	} else if response.Ok {
-		http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-			var update Update
-			var reader io.ReadCloser
-			var err error
+		http.HandleFunc(listenURL, func(w http.ResponseWriter, r *http.Request) {
+			var jsn []byte
 
-			switch request.Header.Get("Content-Encoding") {
+			switch r.Header.Get("Content-Encoding") {
 			case "gzip":
-				reader, err = gzip.NewReader(request.Body)
+				reader, err := gzip.NewReader(r.Body)
 				if err != nil {
 					log.Println(err)
 					return
 				}
 				defer reader.Close()
+				if j, err := io.ReadAll(reader); err == nil {
+					jsn = j
+				} else {
+					log.Println(err)
+				}
 
 			default:
-				reader = request.Body
+				if j, err := io.ReadAll(r.Body); err == nil {
+					jsn = j
+				} else {
+					log.Println(err)
+				}
 			}
 
-			if err = json.NewDecoder(reader).Decode(&update); err != nil {
+			var update Update
+			if err := json.Unmarshal(jsn, &update); err != nil {
 				log.Println(err)
 				return
 			}
 
 			d.updates <- &update
 		})
-		return http.ListenAndServe(fmt.Sprintf(":%d", internalPort), nil)
+		return http.ListenAndServe(fmt.Sprintf("%s:%d", listenURL, internalPort), nil)
 	}
 
-	return errors.New(fmt.Sprintf("could not set webhook: %d %s", response.ErrorCode, response.Description))
+	return fmt.Errorf("could not set webhook: %d %s", response.ErrorCode, response.Description)
 }
