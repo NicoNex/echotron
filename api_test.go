76cffd077241f7388039a596392d117644584bb3
Dj_Mike238 <22237379+DjMike238@users.noreply.github.com>
2021-03-25 15:43:10 +0100

    api_test.go: add more tests
    Added tests for SendSticker, SendChatAction, SetMyCommands, GetMyCommands, SendAnimation and SendAnimationByID

--- api_test.go
+++ api_test.go
@@ -1,6 +1,7 @@
 package echotron
 
 import (
+	"reflect"
 	"testing"
 )
 
@@ -9,11 +10,19 @@ var (
 	api         = NewAPI("1713461126:AAEV5sgVo513Vz4PT33mpp0ZykJqrnSluzM")
 	chatID      = int64(41876271)
 	photoID     = "AgACAgQAAxkDAAMrYFtODxV2LL6-kR_6qSbG9n8dIOIAAti1MRug29lSkNq_9o8PC5uMd7EnXQADAQADAgADbQADeooGAAEeBA"
+	animationID = "CgACAgQAAxkDAAICQGBcoGs7GFJ-tR5AkbRRLFTbvdxXAAJ1CAAC1zHgUu-ciZqanytIHgQ"
 	audioID     = "CQACAgQAAxkDAAIBCmBbamz_DqKk2GmrzmoM0SrzRN6wAAK9CAACoNvZUgPyk-87OM_YHgQ"
 	documentID  = "BQACAgQAAxkDAANmYFtSXcF5kTtwgHeqVUngyuuJMx4AAnQIAAKg29lSb4HP4x-qMT8eBA"
+	stickerID   = "CAACAgIAAxkBAAICImBclqwLQdFHZo15R1zU0vYC1JMFAAImAwACtXHaBj4ZC4vnHBlAHgQ"
 	videoID     = "BAACAgQAAxkDAANxYFtaxF1kfc7nVY_Mtfba3u5dMooAAoYIAAKg29lSpwABJrcveXZlHgQ"
 	videoNoteID = "DQACAgQAAxkDAAIBumBbfT5jPC_cvyEcr0_8DpmFDz2PAALVCgACOX7hUjGZ_MmnZVVeHgQ"
 	voiceID     = "AwACAgQAAxkDAAPXYFtmoFriwJFVGDgPPpfUBljgnYAAAq8IAAKg29lStEWfrNMMAxgeBA"
+
+	commands = []BotCommand{
+		BotCommand{Command: "test1", Description: "Test command 1"},
+		BotCommand{Command: "test2", Description: "Test command 2"},
+		BotCommand{Command: "test3", Description: "Test command 3"},
+	}
 )
 
 func TestGetChat(t *testing.T) {
@@ -252,6 +261,78 @@ func TestSendVoiceByID(t *testing.T) {
 
 func TestSendContact(t *testing.T) {
 	resp, err := api.SendContact("1234567890", "Name", "Surname", chatID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp.Ok {
+		t.Fatal(resp.ErrorCode, resp.Description)
+	}
+}
+
+func TestSendSticker(t *testing.T) {
+	resp, err := api.SendSticker(stickerID, chatID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp.Ok {
+		t.Fatal(resp.ErrorCode, resp.Description)
+	}
+}
+
+func TestSendChatAction(t *testing.T) {
+	resp, err := api.SendChatAction(Typing, chatID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp.Ok {
+		t.Fatal(resp.ErrorCode, resp.Description)
+	}
+}
+
+func TestSetMyCommands(t *testing.T) {
+	resp, err := api.SetMyCommands(commands...)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp.Ok {
+		t.Fatal(resp.ErrorCode, resp.Description)
+	}
+}
+
+func TestGetMyCommands(t *testing.T) {
+	resp, err := api.GetMyCommands()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp.Ok {
+		t.Fatal(resp.ErrorCode, resp.Description)
+	}
+
+	if !reflect.DeepEqual(resp.Result, commands) {
+		t.Logf("expected commands: %v", commands)
+		t.Logf("commands from API: %v", resp.Result)
+		t.Fatal("error: commands mismatch")
+	}
+}
+
+func TestSendAnimation(t *testing.T) {
+	resp, err := api.SendAnimation("tests/animation.mp4", "TestSendAnimation", chatID)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if !resp.Ok {
+		t.Fatal(resp.ErrorCode, resp.Description)
+	}
+}
+
+func TestSendAnimationByID(t *testing.T) {
+	resp, err := api.SendAnimationByID(animationID, "TestSendAnimationByID", chatID)
 	if err != nil {
 		t.Fatal(err)
 	}
