36a50933db0874806e6ef57de43e5d399bb6033e
Nicolò Santamaria <nicolo.santamaria@protonmail.com>
2021-04-16 20:16:30 +0200

    Extend the dispatcher coverage.

--- dispatcher_test.go
+++ dispatcher_test.go
@@ -34,12 +34,13 @@ func TestDelSession(t *testing.T) {
 }
 
 func TestListenWebhook(_ *testing.T) {
-	go dsp.ListenWebhook("example.com")
-	time.Sleep(3 * time.Second)
+	go dsp.ListenWebhook("http://example.com:8443/test")
+	time.Sleep(time.Second)
 }
 
 func TestPoll(_ *testing.T) {
 	go dsp.Poll()
 	dsp.updates <- &Update{}
-	time.Sleep(3 * time.Second)
+	dsp.updates <- &Update{Message: &Message{Chat: &Chat{ID: 0}}}
+	time.Sleep(time.Second)
 }
