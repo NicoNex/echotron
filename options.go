166751d8f57df29f10d56ca82b7c7b35009f289d
Dj_Mike238 <22237379+DjMike238@users.noreply.github.com>
2021-05-31 17:58:32 +0200

    options.go: enable implemented type

--- options.go
+++ options.go
@@ -104,7 +104,7 @@ type InlineKeyboardButton struct {
 	CallbackData                 string   `json:"callback_data,omitempty"`
 	SwitchInlineQuery            string   `json:"switch_inline_query,omitempty"`
 	SwitchInlineQueryCurrentChat string   `json:"switch_inline_query_current_chat,omitempty"`
-	// CallbackGame                 CallbackGame `json:"callback_game,omitempty"`
+	CallbackGame                 CallbackGame `json:"callback_game,omitempty"`
 	Pay bool `json:"pay,omitempty"`
 }
 
