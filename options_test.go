b035a86b5f0449d24cd822811ef676d0fe716de1
Michele Dimaggio <22237379+DjMike238@users.noreply.github.com>
2024-05-25 15:51:51 +0000

    options: add tests

--- options_test.go
+++ options_test.go
@@ -1,6 +1,40 @@
 package echotron
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
+
+var (
+	msgIDOpts = MessageIDOptions{
+		chatID:    1,
+		messageID: 2,
+	}
+
+	inlineMsgIDOpts = MessageIDOptions{
+		inlineMessageID: "inline",
+	}
+)
+
+func TestNewMessageID(t *testing.T) {
+	new := NewMessageID(1, 2)
+
+	if !reflect.DeepEqual(new, msgIDOpts) {
+		t.Logf("expected MessageIDOptions: %+v", msgIDOpts)
+		t.Logf("got MessageIDOptions: %+v", new)
+		t.Fatal("error: MessageIDOptions mismatch")
+	}
+}
+
+func TestNewInlineMessageID(t *testing.T) {
+	new := NewInlineMessageID("inline")
+
+	if !reflect.DeepEqual(new, inlineMsgIDOpts) {
+		t.Logf("expected MessageIDOptions: %+v", inlineMsgIDOpts)
+		t.Logf("got MessageIDOptions: %+v", new)
+		t.Fatal("error: MessageIDOptions mismatch")
+	}
+}
 
 func TestReplyKeyboardMarkupImplementsReplyMarkup(_ *testing.T) {
 	i := ReplyKeyboardMarkup{}
