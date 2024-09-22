db23b7fb65bfb1a6daeaecf92744ff03e37ccf2a
Michele Dimaggio <22237379+DjMike238@users.noreply.github.com>
2024-09-22 21:20:36 +0000

    payments.go: add tests

--- payments_test.go
+++ payments_test.go
@@ -2,6 +2,39 @@ package echotron
 
 import "testing"
 
+func TestRevenueWithdrawalStatePending(t *testing.T) {
+	r := RevenueWithdrawalStatePending{}
+	r.ImplementsRevenueWithdrawalState()
+}
+
+func TestRevenueWithdrawalStateSucceeded(t *testing.T) {
+	r := RevenueWithdrawalStateSucceeded{}
+	r.ImplementsRevenueWithdrawalState()
+}
+
+func TestRevenueWithdrawalStateFailed(t *testing.T) {
+	r := RevenueWithdrawalStateFailed{}
+	r.ImplementsRevenueWithdrawalState()
+}
+
+func TestTransactionPartnerFragment(t *testing.T) {
+	r := TransactionPartnerFragment{}
+	r.ImplementsTransactionPartner()
+}
+
+func TestTransactionPartnerUser(t *testing.T) {
+	r := TransactionPartnerUser{}
+	r.ImplementsTransactionPartner()
+}
+func TestTransactionPartnerTelegramAds(t *testing.T) {
+	r := TransactionPartnerTelegramAds{}
+	r.ImplementsTransactionPartner()
+}
+func TestTransactionPartnerOther(t *testing.T) {
+	r := TransactionPartnerOther{}
+	r.ImplementsTransactionPartner()
+}
+
 func TestSendInvoice(t *testing.T) {
 	_, err := api.SendInvoice(
 		chatID,
