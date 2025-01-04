3260f8fa8a077af9086d3b4594e2372d32643fbd
Michele Dimaggio <22237379+DjMike238@users.noreply.github.com>
2025-01-04 14:45:20 +0000

    chatadminrights.go: fix derp

--- chatadminrights.go
+++ chatadminrights.go
@@ -20,21 +20,21 @@ package echotron
 
 // ChatAdministratorRights represents the rights of an administrator in a chat.
 type ChatAdministratorRights struct {
-	IsAnonymous          bool `json:"is_anonymous"`
-	CanManageChat        bool `json:"can_manage_chat"`
-	CanDeleteMessages    bool `json:"can_delete_messages"`
-	CanManageVideo_chats bool `json:"can_manage_video_chats"`
-	CanRestrictMembers   bool `json:"can_restrict_members"`
-	CanPromoteMembers    bool `json:"can_promote_members"`
-	CanChangeInfo        bool `json:"can_change_info"`
-	CanInviteUsers       bool `json:"can_invite_users"`
-	CanPostMessages      bool `json:"can_post_messages,omitempty"`
-	CanEditMessages      bool `json:"can_edit_messages,omitempty"`
-	CanPinMessages       bool `json:"can_pin_messages,omitempty"`
-	CanPostStories       bool `json:"can_post_stories,omitempty"`
-	CanEditStories       bool `json:"can_edit_stories,omitempty"`
-	CanDeleteStories     bool `json:"can_delete_stories,omitempty"`
-	CanManageTopics      bool `json:"can_manage_topics,omitempty"`
+	IsAnonymous         bool `json:"is_anonymous"`
+	CanManageChat       bool `json:"can_manage_chat"`
+	CanDeleteMessages   bool `json:"can_delete_messages"`
+	CanManageVideoChats bool `json:"can_manage_video_chats"`
+	CanRestrictMembers  bool `json:"can_restrict_members"`
+	CanPromoteMembers   bool `json:"can_promote_members"`
+	CanChangeInfo       bool `json:"can_change_info"`
+	CanInviteUsers      bool `json:"can_invite_users"`
+	CanPostMessages     bool `json:"can_post_messages,omitempty"`
+	CanEditMessages     bool `json:"can_edit_messages,omitempty"`
+	CanPinMessages      bool `json:"can_pin_messages,omitempty"`
+	CanPostStories      bool `json:"can_post_stories,omitempty"`
+	CanEditStories      bool `json:"can_edit_stories,omitempty"`
+	CanDeleteStories    bool `json:"can_delete_stories,omitempty"`
+	CanManageTopics     bool `json:"can_manage_topics,omitempty"`
 }
 
 // SetMyDefaultAdministratorRightsOptions contains the optional parameters used by
